8f63ae6b10a5465510e25305a756e0348447055e
devilmaff <devilblackkinghusain.com>
2025-08-23 10:58:47 +0530

    m2

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,3 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const User = require('../../models/User');
+
 // @route   POST api/auth/login
 // @desc    Authenticate user & get token
 // @access  Public
