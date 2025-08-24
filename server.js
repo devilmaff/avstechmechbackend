668f7fe36155a902fe4148b817c97d3ef3639eb3
devilmaff <devilblackkinghusain.com>
2025-08-24 15:53:00 +0530

    Fix app initialization order

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const http = require('http');
 const { Server } = require("socket.io");
 const connectDB = require('./config/db');
 const cors = require('cors');
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/messages', require('./routes/api/messages'));
 
 // Initialize Express app
 const app = express();
@@ -24,9 +22,7 @@ const io = new Server(server, {
 connectDB();
 
 // --- Middleware ---
-// Enable Cross-Origin Resource Sharing
 app.use(cors());
-// Enable Express to parse JSON request bodies
 app.use(express.json({ extended: false }));
 
 // --- API Routes ---
@@ -38,20 +34,16 @@ app.use('/api/messages', require('./routes/api/messages'));
 io.on('connection', (socket) => {
   console.log('A user connected:', socket.id);
 
-  // Listen for new messages from a client
   socket.on('sendMessage', (message) => {
     console.log('Message received:', message);
-    // Broadcast the new message to all connected clients
     io.emit('receiveMessage', message);
   });
-  
-  // Listen for a message edit event
+
   socket.on('editMessage', (message) => {
     console.log('Message edited:', message);
     io.emit('messageEdited', message);
   });
-  
-  // Listen for a message delete event
+
   socket.on('deleteMessage', (messageId) => {
     console.log('Message deleted:', messageId);
     io.emit('messageDeleted', messageId);
@@ -61,7 +53,6 @@ io.on('connection', (socket) => {
     console.log('User disconnected:', socket.id);
   });
 });
-
 
 // Define the port to run the server on
 const PORT = process.env.PORT || 5000;
