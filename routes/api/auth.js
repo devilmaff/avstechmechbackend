32b65cf467fc98a193f28e7c272320e2086369fd
devilmaff <devilblackkinghusain.com>
2025-08-24 15:48:34 +0530

    k

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -73,3 +73,4 @@ router.post('/login', async (req, res) => {
     res.status(500).send('Server error');
   }
 });
+module.exports = router;
--- routes/api/messages.js
+++ routes/api/messages.js
@@ -104,5 +104,4 @@ router.delete('/:id', auth, async (req, res) => {
     }
 });
 
-
 module.exports = router;
